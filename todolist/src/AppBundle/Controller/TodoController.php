e282c228717e89cc94891fb0dba2ba99bb6f8f59
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-09 16:19:51 +0100

    Create a controller, todo list

---
+++ todolist/src/AppBundle/Controller/TodoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * @Route("/todos", name="todo_list")
+     */
+    public function listAction(Request $request)
+    {
+
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig');
+    }
+}
